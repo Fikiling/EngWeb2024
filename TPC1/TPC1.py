13e45ffe91e3b3b9c7edb92df6d0e9cea59b9b10
Fikiling <fsg.filipegoncalves@gmail.com>
2024-02-24 00:39:42 +0000

    quase

--- TPC1/TPC1.py
+++ TPC1/TPC1.py
@@ -1,19 +1,29 @@
 import os 
 from xml.etree import ElementTree as ET
+from lxml import etree
 
 
 os.mkdir('html')
 html_folder = "html/"
 
 xml_folder = "../../Sets/MapaRuas-materialBase/texto"
-xsd_path = "../../Sets/MapaRuas-materialBaseMRB-rua.xsd"
+xsd_path = "../../Sets/MapaRuas-materialBase/MRB-rua.xsd"
 
 
-def validate_xml(xml_path):
-    xml_tree = ET.parse(xml_path)
-    xsd_root = ET.parse(xsd_path).getroot()  
-    xml_schema = ET.XMLSchema(xsd_root)
-    return xml_schema.validate(xml_tree)
+def validate_xml(xml_path, xsd_path):
+    try:
+        with open(xsd_path, 'r') as xsd_file:
+            schema = etree.XMLSchema(etree.parse(xsd_file))
+        with open(xml_path, 'r') as xml_file:
+            xml = etree.parse(xml_file)
+        schema.assertValid(xml)
+        return True
+    except etree.XMLSchemaError as e:
+        print(f"Erro no esquema XSD: {e}")
+        return False
+    except etree.DocumentInvalid as e:
+        print(f"Documento XML inválido: {e}")
+        return False
 
 
 html = '''
@@ -28,48 +38,94 @@ html = '''
 '''
 
 template = html
+html += '<ul>'
 
-info_ruas = {}
+info_ruas = []
 for xml_filename in os.listdir(xml_folder):
     if xml_filename.endswith ('.xml'):
         xml_path = os.path.join(xml_folder, xml_filename)
-        if validate_xml(xml_path):
+        if validate_xml(xml_path, xsd_path):
             tree = ET.parse(xml_path)
             root = tree.getroot()
              
-
+            
             nome_rua = root.find("./meta/nome").text
-            descricao = root.find("./corpo/para").text
-            populacao = root.find("./meta/população").text
+            numero_rua = root.find("./meta/número").text
+            descricao = ""
+            for para_elem in root.findall("./corpo/para"):
+                if para_elem.text is not None:
+                    descricao += para_elem.text.strip() + " "
+
+            
 
             casas = []
             for casa_elem in root.findall("./corpo/lista-casas/casa"):
-                numero = casa_elem.find("número").text
-                enfiteuta = casa_elem.find("enfiteuta").text
-                foro = casa_elem.find("foro").text
-                desc = casa_elem.find("desc").text
-                casas.append({"numero": numero, "enfiteuta": enfiteuta, "foro": foro, "desc": desc})
+                numero_elem = casa_elem.find("número")
+                numero = numero_elem.text if numero_elem is not None else "N/A"
+                
+                enfiteuta_elem = casa_elem.find("enfiteuta")
+                enfiteuta = enfiteuta_elem.text if enfiteuta_elem is not None else "N/A"
+                
+                foro_elem = casa_elem.find("foro")
+                foro = foro_elem.text if foro_elem is not None else "N/A"
+                
+                desc_casa = ""
+                desc_elem = casa_elem.find("desc")
+                if desc_elem is not None:
+                    for para_elem in desc_elem.findall("para"):
+                        desc_text = para_elem.text
+                        if desc_text is not None:
+                            desc_casa += desc_text.strip() + " "
+                
+                casas.append({"numero": numero, "enfiteuta": enfiteuta, "foro": foro, "desc": desc_casa})
 
-            
-            info_ruas[nome_rua] = {
-                "nome": nome_rua,
-                "descricao": descricao,
-                "populacao": populacao,
-                # Adicione aqui quaisquer outras informações que desejar extrair do XML
-                "casas": casas
-            }
 
-for elem in info_ruas
-            
+            imagens = []
+            for figura_elem in root.findall("./corpo/figura"):
+                imagem_path = figura_elem.find("imagem").get("path")
+                legenda = figura_elem.find("legenda").text
+                imagens.append({"path": imagem_path, "legenda": legenda})
 
+            templateRua = template 
+            templateRua += f'<h1> {nome_rua} </h1>' 
+            templateRua += f'<h3><b> Número de Rua: {numero_rua} </b></h3>'
+            templateRua += f'<h3><b> Lista de Casas: </b></h3>'
+            for casa in casas:
+                templateRua += "<div>"
+                templateRua += f"<p>Casa com o número: {casa['numero']}</p>"
+                templateRua += f"<p>Donos da casa: {casa['enfiteuta']}</p>"
+                templateRua += f"<p>Foro: {casa['foro']}</p>"
+                templateRua += f"<p>Descrição: {casa['desc']}</p>"
+                templateRua += "</div>"
 
+            templateRua += f'<p><b>Descrição:</b> {descricao} </p>'
+            for imagem_info in imagens:
+                templateRua += f'<img src="{imagem_info["path"]}" alt="{imagem_info["legenda"]}">'
+                templateRua += f'<p>{imagem_info["legenda"]}</p>'
+            templateRua += f"<h6><a href='../mapa_sorted_linked.html'>Voltar</a></h6>"
+            templateRua += "</body>"
+            templateRua += "</html"
 
-with open("mapa_ruas.html", "w", encoding="utf-8") as main_html_file:
+            info_ruas.append(nome_rua)
+
+            fileRua = open(f"html/{nome_rua}.html", "w", encoding="utf-8")
+            fileRua.write(templateRua)
+            fileRua.close()
     
-    main_html_file.write(html_content)
+for elem in sorted(info_ruas):
+    html += f'<li><a href="html/{elem}.html">{elem}</li>'
 
 
 
+html += "</ul>"
+html += "</body>"
+html += "</hmtl>"
+
+file = open("mapa_sorted_linked.html", "w", encoding="utf-8")
+file.write(html)
+file.close()
 
 
-html += "<ul>"
+
+            
+
