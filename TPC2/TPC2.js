b40fb12ba93bf618f524bb03ee4539ce5f67a7f7
Fikiling <a100696@uminho.pt>
2024-02-29 12:41:27 +0000

    TPC2 quase feito

---
+++ TPC2/TPC2.js
@@ -0,0 +1,5 @@
+var http = require('http')
+var url = require('url')
+var axios = require('axios')
+
+http.createServer(function(req,res){
---
+++ TPC2/TPC2.py
@@ -0,0 +1,78 @@
+import json
+import os
+
+
+def getNome(id):
+    for cidade in content['cidades']:
+        if cidade['id'] == id:
+            return cidade['nome']
+    return None
+
+html= '''
+<!DOCTYPE html>
+<html>
+<head>
+    <title> EngWeb2024 TPC2</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="w3.css"/>
+
+</head>
+<body>
+    <h1><b>Mapa Virtual</b></h1>
+''' 
+
+if not os.path.exists('html'):
+    os.mkdir('html')
+
+with open('mapa-virtual.json', 'r', encoding='utf-8') as file:
+    content = json.load(file)
+
+template = html
+html += '<ul class="w3-ul w3-hoverable">'
+
+
+
+ligacoes = {}
+for ligacao in content['ligacoes']:
+    nome_destino = getNome(ligacao['destino'])
+    tuplo = (nome_destino, ligacao['distância'])
+    
+    if ligacao['origem'] in ligacoes:
+        ligacoes[ligacao['origem']].append(tuplo)  
+    else:
+        ligacoes[ligacao['origem']] = [tuplo]
+
+
+listaCidades = []
+for cidade in content['cidades']:
+    listaCidades.append(cidade['nome'])
+    
+    
+    templateCidade = template
+    templateCidade += f'<h1>Nome: {cidade["nome"]}</h1>'
+    templateCidade += f'<h3>ID: {cidade["id"]}</h3>'
+    templateCidade += f'<p><b>Distrito: {cidade["distrito"]}</b></p>'
+    templateCidade += f'<p><b>Descrição: {cidade["descrição"]}</b></p>'
+    templateCidade += f'<p><b>População: {cidade["população"]}</b></p>'
+    if ligacoes[ligacoes['id']] in  ligacoes:
+        for elem in ligacoes[ligacoes['id']]:
+            templateCidade += f'<p><b>Ligação: <a href="html/{elem[0]}style="text-decoration: none;">  - Distância: {elem[1]}</b></p>'
+
+    templateCidade += f'<h6><a href="../index.html">Voltar à página principal</a></h6>'
+    templateCidade += '</body>'
+    templateCidade += '</html>'
+
+    fileCidade = open(f'html/{cidade["nome"]}.html', 'w', encoding='utf-8')
+    fileCidade.write(templateCidade)
+    fileCidade.close()
+
+for elem in sorted(listaCidades):
+    html += f'<li class="w3-hover-green"><a href="html/{elem}.html" style="text-decoration: none;">{elem}</li>'   
+
+html += '</ul>'
+html += '</body>'
+html += '</html>'
+
+file = open('index.html', 'w', encoding='utf-8')
+file.write(html)
+file.close()
