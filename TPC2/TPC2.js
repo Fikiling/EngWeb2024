fbf2e791c7f9c585ff5ae71ab293ceb49e90d202
Fikiling <a100696@uminho.pt>
2024-03-02 19:00:57 +0000

    TPC2 finalizado

--- TPC2/TPC2.js
+++ TPC2/TPC2.js
@@ -4,8 +4,10 @@ var fs = require('fs')
 
 http.createServer((req, res) => {
     console.log(req.method + ' ' + req.url) 
-    
-    if (req.url == '/'){
+    var q = url.parse(decodeURI(req.url), true);
+    var re = /^\/html\/[A-Za-z\sÀ-ú%20-]+\.html$/;
+
+    if (q.pathname == '/' || q.pathname == '/index.html'){
         fs.readFile('index.html', (erro, dados) => {
             res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
             res.write(dados)
@@ -13,6 +15,27 @@ http.createServer((req, res) => {
         }
         )
     }
-    else if (req.url ==){
+    else if (re.test(q.pathname )){
+        fs.readFile(decodeURIComponent(q.pathname.substring(1)), (erro, dados) => {
+            res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+            res.write(dados);
+            res.end();
+        });
         
     }
+    else if(q.pathname == "/w3.css"){
+        fs.readFile("w3.css",function(err,data){
+            res.writeHead(200,{"Content-Type": "text/css"})
+            res.write(data)
+            res.end()
+        }) 
+
+    }
+    else {
+        res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
+        res.write("<p>Pedido não suportado: " + req.method + " " + req.url + "</p>")
+        res.end()
+    }
+
+}).listen(7777)
+console.log('Servidor à escuta na porta 7777...')   
